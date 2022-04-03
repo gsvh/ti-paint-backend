ec6657092973778e8f406c7a155b347d8bb8bf56
George van Heerden <george.vanheerden@sovtech.com>
2022-04-04 01:46:00 +0200

    fix(env/production): updated server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env(MY_HEROKU_URL),
-  proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS", ["a31f2044fe3e7976", "fe5a456f6d5d6b95"]),
+    keys: env.array("APP_KEYS", "a31f2044fe3e7976", "fe5a456f6d5d6b95"),
   },
 });
