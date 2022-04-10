c6f61a2ba217bc3f78048b7fa550aa1a50e7118a
George van Heerden <george.vanheerden@sovtech.com>
2022-04-10 16:15:16 +0200

    fix(env/production): updated server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS", "a31f2044fe3e7976", "fe5a456f6d5d6b95"),
-  },
+  url: env("URL"),
 });
